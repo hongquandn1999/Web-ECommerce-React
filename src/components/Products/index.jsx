ea35759a961858330ffdc605d5c02190c9f5e74a
misuko1999 <hhq85@gmail.com>
2021-03-06 22:01:57 +0700

    Mock Product 1

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -2,11 +2,7 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import Product from '../Product';
 
-const products = [
-	{ id: 1, name: 'Minh Trang', description: 'Trần Thị Minh Trang' },
-	{ id: 2, name: 'Xuân Hồng', description: 'Ngô Trần Xuân Hồng' },
-	{ id: 3, name: 'Quỳnh Anh', description: 'Nguyễn Lê Quỳnh Anh' },
-];
+const products = [];
 
 const Products = (props) => {
 	return (
