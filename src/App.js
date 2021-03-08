ef4e27c1c8ec6d3ec5e3f9db8072012dd429a8f0
misuko1999 <hhq85@gmail.com>
2021-03-08 15:22:50 +0700

    completed handle add to cart and change ui icon shopping cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { commerce } from './lib/Commerce';
 import { Products, NavBar } from './components';
 function App() {
 	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
 
 	const fetchProducts = async () => {
 		const { data } = await commerce.products.list();
@@ -10,17 +11,29 @@ function App() {
 		setProducts(data);
 	};
 
+	const fetchCart = async () => {
+		setCart(await commerce.cart.retrieve());
+	};
+
+	const handleAddToCart = async (productId, quantity) => {
+		const response = await commerce.cart.add(productId, quantity);
+
+		setCart(response.cart);
+	};
+
 	useEffect(() => {
 		fetchProducts();
+		fetchCart();
 	}, []);
 
 	console.log(products);
+	console.log(cart);
 
 	return (
 		<div>
 			<h1>List Products</h1>
-			<NavBar />
-			<Products products={products} />
+			<NavBar totalItem={cart.total_items} />
+			<Products products={products} onAddToCart={handleAddToCart} />
 		</div>
 	);
 }
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -5,13 +5,13 @@ import Product from '../Product';
 
 
 const Products = (props) => {
-	const { products } = props;
+	const { products, onAddToCart } = props;
 	return (
 		<main>
 			<Grid container spacing={4} justify='center'>
 				{products.map((item) => (
 					<Grid item key={item.id} xs={12} sm={6} md={4} lg={3}>
-						<Product product={item} />
+						<Product product={item} onAddToCart={onAddToCart} />
 					</Grid>
 				))}
 			</Grid>
