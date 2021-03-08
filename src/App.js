01cc4d0962b5fb709aa5c977baa9399781a3688f
misuko1999 <hhq85@gmail.com>
2021-03-08 14:35:51 +0700

    Get API product from commercejs and display to web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { commerce } from './lib/Commerce';
 import { Products, NavBar } from './components';
 function App() {
+	const [products, setProducts] = useState([]);
+
+	const fetchProducts = async () => {
+		const { data } = await commerce.products.list();
+
+		setProducts(data);
+	};
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	console.log(products);
+
 	return (
 		<div>
 			<h1>List Products</h1>
 			<NavBar />
-			<Products />
+			<Products products={products} />
 		</div>
 	);
 }
